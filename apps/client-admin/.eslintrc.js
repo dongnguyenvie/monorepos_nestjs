6c1b25dacabaed6fd743406b791c489ac429ab66
Nolan <dongnguyenvie@gmail.com>
2023-02-09 03:31:23 +0700

    solve conficted eslint, codegen from admin source

--- apps/client-admin/.eslintrc.js
+++ apps/client-admin/.eslintrc.js
@@ -1,3 +1,8 @@
+const fs = require('fs')
+const path = require('path')
+
+const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'))
+
 module.exports = {
   root: true,
   extends: ['custom'],
