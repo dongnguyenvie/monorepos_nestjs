6096c35353daeb5c8127347cf53712bcf0419b63
Nolan <dongnguyenvie@gmail.com>
2023-02-12 11:20:57 +0700

    no message

--- apps/client/src/routes/+page.svelte
+++ apps/client/src/routes/+page.svelte
@@ -9,28 +9,6 @@
 	<meta name="description" content="homepage" />
 </svelte:head>
 
-<!-- <section>
-	<RoomCard
-		name="Hà Kiều Oanh"
-		title="Oanh Cần người  trò chuyện, mới cãi nhau với bạn trai"
-		avatar="https://www.w3schools.com/w3images/avatar2.png"
-		tags={[
-			'Tình yêu',
-			'Bản thân',
-			'Coffee',
-			'Du lịch',
-			'Tình yêu',
-			'Bản thân',
-			'Coffee',
-			'Du lịch'
-		]}
-		onClick={() => {}}
-		emotions={['1', '2', '3']}
-		id="xxx"
-		isFull
-	/>
-</section> -->
-
 <Redirect to={ROUTES.rooms} />
 
 <style>
